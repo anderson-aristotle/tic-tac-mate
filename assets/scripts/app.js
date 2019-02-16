de55dd823324115b61f142d24c3bf80bea6c4dd8
rex <anderson.aristotle@gmail.com>
2019-02-16 17:43:08 -0500

    added game board and clickable objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,17 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const authEvents = require('./auth/events.js')
+const gameEvents = require('./game-events/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // console.log('Game events is', gameEvents)
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-form').on('submit', authEvents.onSignOut)
+  $('.box').on('click', gameEvents.onBoxClick)
 })
