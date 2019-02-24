421c9abf4d3da984419435ffd8bcd63fa583183e
rex <anderson.aristotle@gmail.com>
2019-02-24 06:23:19 -0500

    #added gameBoard rules, so players cannot click on same box #and player turns do not loop

--- assets/scripts/game/game-events.js
+++ assets/scripts/game/game-events.js
@@ -7,34 +7,50 @@ const ui = require('./game-ui.js')
 
 const winConditions = [
   [0, 1, 2], [3, 4, 5],
-  [6, 8, 7], [0, 3, 6],
+  [6, 7, 8], [0, 3, 6],
   [1, 4, 7], [2, 5, 8],
   [0, 4, 8], [6, 4, 2]
 ]
 
 const gameBoard = ['', '', '', '', '', '', '', '', '']
 
-const createGame = (event) => {
-  event.preventDefault()
+const createGame = () => {
   api.createGame()
     .then(ui.createGameSuccess)
     .catch()
 }
 
 store.currentPlayer = 'X'
+const over = false
 
 const switchPlayer = (event) => {
   event.preventDefault()
+  const index = $(event.target).data('cell-index')
+
   if (store.currentPlayer === 'X') {
     $(event.target).text('X')
+    gameBoard[index] = 'X'
+    $(event.target).off('click')
     store.currentPlayer = 'O'
   } else if (store.currentPlayer === 'O') {
     $(event.target).text('O')
+    gameBoard[index] = 'O'
+    $(event.target).off('click')
     store.currentPlayer = 'X'
   }
+
+  // check winning win conditions
+
+  winConditions.forEach(cond => {
+    if (gameBoard[cond[0]] && gameBoard[cond[0]] === gameBoard[cond[1]] && gameBoard[cond[1]] === gameBoard[cond[2]]) {
+      console.log('someone won')
+      $('.box').off('click')
+      // over = true
+    }
+  })
+
   const value = $(event.target).text()
-  const index = $(event.target).data('cell-index')
-  const over = false // <--- change in future
+
   api.updateGameBoard(value, index, over)
     .then((data) => console.log('This works!!!'))
     .catch(ui.failure)
