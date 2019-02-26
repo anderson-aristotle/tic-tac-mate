8643da41239e29163e896ff9715b3bb95dd9b8c2
rex <anderson.aristotle@gmail.com>
2019-02-25 20:01:21 -0500

    added createNewGame =>; store player and gamebaord state; updated game api to store game information

--- assets/scripts/game/game-events.js
+++ assets/scripts/game/game-events.js
@@ -5,21 +5,24 @@ const store = require('../store.js')
 const api = require('./game-api.js')
 const ui = require('./game-ui.js')
 
+const createNewGame = () => {
+  event.preventDefault()
+  api.createNewGame()
+    .then(ui.createGameSuccess)
+    .catch(ui.failure)
+  // $('#playerOne').html(store.user.email + ' ' + playerOneCount + ' wins')
+  // $('#playerTwo').html('User Two ' + playerTwoCount + ' wins')
+}
+
+const gameBoard = ['', '', '', '', '', '', '', '', '']
+
 const winConditions = [
   [0, 1, 2], [3, 4, 5],
   [6, 7, 8], [0, 3, 6],
   [1, 4, 7], [2, 5, 8],
   [0, 4, 8], [6, 4, 2]
 ]
-
-const gameBoard = ['', '', '', '', '', '', '', '', '']
-
-const createGame = () => {
-  api.createGame()
-    .then(ui.createGameSuccess)
-    .catch()
-}
-
+const currentPlayer = 'X'
 store.currentPlayer = 'X'
 const over = false
 
@@ -27,38 +30,56 @@ const switchPlayer = (event) => {
   event.preventDefault()
   const index = $(event.target).data('cell-index')
 
+  if ($(event.target).text()) {
+    // setTimeOut(() => {
+    $('#user-feedback').text('Select An Empty Square!')
+    //  }, 1000)
+    return
+  }
+  if (gameBoard.winner) { return }
+
+  winConditions.forEach(cond => {
+    if (gameBoard[cond[0]] && gameBoard[cond[0]] === gameBoard[cond[1]] && gameBoard[cond[1]] === gameBoard[cond[2]]) {
+    }
+    if (winConditions === true) {
+      gameBoard.winner = currentPlayer
+    } if (gameBoard.turnCounter === 9 && gameBoard.winner === 'undefined') {
+      gameBoard.winner = 'Tie'
+    } if (gameBoard.winner !== undefined) {
+      gameBoard.api.gameover = true
+    }
+  })
+
   if (store.currentPlayer === 'X') {
     $(event.target).text('X')
     gameBoard[index] = 'X'
-    $(event.target).off('click')
     store.currentPlayer = 'O'
   } else if (store.currentPlayer === 'O') {
     $(event.target).text('O')
     gameBoard[index] = 'O'
-    $(event.target).off('click')
     store.currentPlayer = 'X'
   }
 
-  // check winning win conditions
-
-  winConditions.forEach(cond => {
-    if (gameBoard[cond[0]] && gameBoard[cond[0]] === gameBoard[cond[1]] && gameBoard[cond[1]] === gameBoard[cond[2]]) {
-      console.log('someone won')
-      $('.box').off('click')
-      // over = true
-    }
-  })
-
   const value = $(event.target).text()
 
-  api.updateGameBoard(value, index, over)
-    .then((data) => console.log('This works!!!'))
+  api.updateGameBoard(index, value, over)
+    .then(ui.switchPlayerSuccess)
     .catch(ui.failure)
+
+  console.log(store)
 }
+
+//
+// const createGame = () => {
+//   event.preventDefault()
+//   $('#reset-game').on('').reset()
+//   // new gameboard
+//   $()
+// }
 
 module.exports = {
   switchPlayer,
-  createGame,
+  createNewGame,
   gameBoard,
   winConditions
 }
