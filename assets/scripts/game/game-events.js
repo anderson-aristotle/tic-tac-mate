290db002c2a4a595bff8b1d7ff173f69d1bf187c
rex <anderson.aristotle@gmail.com>
2019-02-27 13:10:24 -0500

    practice branch -- game logic test

--- assets/scripts/game/game-events.js
+++ assets/scripts/game/game-events.js
@@ -1,6 +1,4 @@
 'use strict'
-
-// events to handle the click
 const store = require('../store.js')
 const api = require('./game-api.js')
 const ui = require('./game-ui.js')
@@ -22,10 +20,17 @@ const winConditions = [
   [1, 4, 7], [2, 5, 8],
   [0, 4, 8], [6, 4, 2]
 ]
+
 const currentPlayer = 'X'
 store.currentPlayer = 'X'
 const over = false
 
+if (gameBoard === over) {
+  console.log('gameBoard' + 'is empty!')
+  // api.updateGameBoard(index, value, over)
+  //   .then(ui.switchPlayerSuccess)
+  //   .catch(ui.failure)
+}
 const switchPlayer = (event) => {
   event.preventDefault()
   const index = $(event.target).data('cell-index')
@@ -60,12 +65,6 @@ const switchPlayer = (event) => {
     store.currentPlayer = 'X'
   }
 
-  const value = $(event.target).text()
-
-  api.updateGameBoard(index, value, over)
-    .then(ui.switchPlayerSuccess)
-    .catch(ui.failure)
-
   console.log(store)
 }
 
@@ -83,3 +82,143 @@ module.exports = {
   gameBoard,
   winConditions
 }
+// events to handle the click
+//
+// const createNewGame = () => {
+// event.preventDefault()
+// api.createNewGame()
+//   .then(ui.createGameSuccess)
+//   .catch(ui.failure)
+// }
+//
+// const gameBoard = ['', '', '', '', '', '', '', '', '']
+//
+// const winConditions = [
+// [0, 1, 2], [3, 4, 5],
+// [6, 7, 8], [0, 3, 6],
+// [1, 4, 7], [2, 5, 8],
+// [0, 4, 8], [6, 4, 2]
+// ]
+// // winConditions.forEach(cond => {
+// //   if (gameBoard[cond[0]] ===
+// //     && gameBoard[cond[0]]
+//
+// //     === gameBoard[cond[1]]
+// //     && gameBoard[cond[1]]
+//
+// //     === gameBoard[cond[2]]) {
+// //  }   && gameBoard[cond[2]]
+//
+// //   === gameBoard[cond[3]]) {
+// //  }   && gameBoard[cond[3]]
+//
+// //    === gameBoard[cond[4]]) {
+// //  }   && gameBoard[cond[4]]
+//
+// //    === gameBoard[cond[5]]) {
+// //  }   && gameBoard[cond[5]]
+//
+// //    === gameBoard[cond[6]]) {
+// //  }   && gameBoard[cond[6]]
+//
+// //    === gameBoard[cond[7]]) {
+// //  }   && gameBoard[cond[7]]
+//
+// //    === gameBoard[cond[8]]) {
+// //  }   && gameBoard[cond[8]]
+//
+// //  === gameBoard[cond[9]]) {
+// //  }   && gameBoard[cond[9]]
+// // })
+//
+// const currentPlayer = 'X'
+// store.currentPlayer = 'X'
+// const over = false
+//
+// // if (gameBoard.winner) = {
+// function checkWin() {
+// if (gameboard[cond[0].textContent === "X" &&
+// gameBoard[cond[1]].textContent === "X" &&
+// gameBoard[cond[2]].textContent === "X")
+// { alert("Win")}
+// else if (
+// gameBoard[cond[3]].textContent === "X" &&
+// gameBoard[cond[4]].textContent === "X" &&
+// gameBoard[cond[5]].textContent === "X")
+// { alert("Win")}
+// else if (
+// gameBoard[cond[6]].textContent === "X" &&
+// gameBoard[cond[7]].textContent === "X" &&
+// gameBoard[cond[8]].textContent === "X"
+// ) { alert("Win")}
+// }
+//
+// if (winConditions === true) {
+//   gameBoard.winner = currentPlayer
+// } if (gameBoard.turnCounter === 9 && gameBoard.winner === 'undefined') {
+//   gameBoard.winner = 'Tie'
+// } if (gameBoard.winner !== undefined) {
+//   gameBoard.api.gameover = true
+// }
+//
+// const switchPlayer = (event) => {
+// event.preventDefault()
+// }
+// const index = $(event.target).data('cell-index')
+//
+// if ($(event.target).text()) {
+//   // setTimeOut(() => {
+//   $('#user-feedback').text('Select An Empty Square!')
+//   //  }, 1000)
+//   // return
+// }
+//
+// if (store.currentPlayer === 'X') {
+//   $(event.target).text('X')
+//   gameBoard[index] = 'X'
+//   store.currentPlayer = 'O'
+// } else if (store.currentPlayer === 'O') {
+//   $(event.target).text('O')
+//   gameBoard[index] = 'O'
+//   store.currentPlayer = 'X'
+// }
+//
+// const playerTurn = () => {
+//   if (currentPlayer === 'X') {
+//     return $('#user-message').text('X: it is your turn')
+//   } else if (currentPlayer === 'O') {
+//     return $('#user-message').text('O: it is your turn')
+//   }
+// }
+// playerTurn()
+//
+// const clickError = () => {
+//   $('#user-message').text('Error, space is taken!')
+//   setTimeout(() => {
+//     $('#user-message').text('')
+//     $('#user-message').text(currentPlayer + ': it is your turn')
+//   }, 1500)
+// }
+// const gameOverError = () => {
+//   ui.gameIsOver()
+// }
+//
+// const value = $(event.target).text()
+//
+//
+//
+// console.log(store)
+//
+// const createGame = () => {
+//   event.preventDefault()
+//   $('#create-game').show('#game-board')
+//   $('#reset-game').on('').reset()
+// }
+//
+// module.exports = {
+//   switchPlayer,
+//   createNewGame,
+//   gameBoard,
+//   winConditions,
+//   createGame
+// }
--- assets/scripts/game/game-ui.js
+++ assets/scripts/game/game-ui.js
@@ -1,7 +1,15 @@
 'use strict'
 const store = require('../store.js')
 
+// const [variable] = event => {
+//   gameLogic.gameBoard = gameLogic.makeBoard(3)
+//   $('.cell').removeClass('x')
+//   $('.cell').removeClass('o')
+//   gameUi.displayUpdate()
+// }
+
 const createGameSuccess = (gameResponse) => {
+  console.log(gameResponse)
   $('user-message').text('Begin Game')
   console.log(gameResponse.game)
   store.game = gameResponse.game
@@ -9,7 +17,75 @@ const createGameSuccess = (gameResponse) => {
   console.log(store)
 }
 
+// const newGame = () => {
+//   gameApi.createNewGame()
+//     .then(gameLogic.assignID)
+//   gameLogic.gameBoard = gameLogic.makeBoard(3)
+//   $('.cell').removeClass('x')
+//   $('.cell').removeClass('o')
+//   $('#game-display').stop(true, true)
+//   $('#game-display').fadeTo('fast', 1)
+//   gameUi.displayUpdate()
+//   onGetGames()
+// }
+
+// const onMove = event => {
+// if (gameLogic.gameBoard.winner === undefined) {
+// gameLogic.takeTurn(event.target.id)
+// gameUi.displayUpdate()
+// gameUi.boardUpdate()
+// gameApi.updateGame()
+// } else {
+// gameUi.alreadyOver()
+//   }
+// }
+
+const switchPlayerSuccess = data => {
+  console.log('this data comes from switch player success')
+  console.log(data)
+  store.game.cells = data.game.cells
+}
+
+const userFeedBack = () => {
+  $('user-feedback').on()
+}
+
+const signInSuccess = (data) => {
+  $('#user-message').text('Successfully signed Sign In')
+  $('.before-sign-in').hide()
+  $('.after-sign-in').show()
+  store.user = data.user
+}
+
+const resetGame = () => {
+  $('#user-message').text('Congrats, game is reset!')
+}
 // console.log('store:', store)
+
+// const onGetGames = () => {
+//   gameApi.getGames()
+//     .then(gameUi.onGetGamesSuccess)
+//     .catch(gameUi.onGetGamesFailure)
+// }
+// const onGetGameById = (event) => {
+//   event.preventDefault()
+//   const form = event.target
+//   const formData = getFormFields(form)
+//   gameApi.getGameById(formData)
+//     .then(gameUi.getGameByIdSuccess)
+//     .catch(gameUi.getGameByIdfailure)
+// }
+
+// const onGetAllGames = () => {
+//   gameApi.getAllGames()
+//   .then(gameUi.onGetAllGamesSuccess)
+//   .catch(gameUi.onGetGamesFailure)
+// }
+
 module.exports = {
-  createGameSuccess
+  createGameSuccess,
+  resetGame,
+  switchPlayerSuccess,
+  userFeedBack,
+  signInSuccess
 }
