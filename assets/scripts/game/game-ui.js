042f6c3076fdbbfd2a32285a5c45a5547054c6a5
Charlie <charliekelly31415@gmail.com>
2019-02-27 22:22:10 -0500

    Change user-message ID
    there is no #user-message element, only #user-feedback, so all user messages were failing to display

--- assets/scripts/game/game-ui.js
+++ assets/scripts/game/game-ui.js
@@ -1,22 +1,29 @@
 'use strict'
 const store = require('../store.js')
 
-const createGameSuccess = (gameResponse) => {
-  $('#user-message').text('Begin Game')
-  store.game = gameResponse.game
+const createGameSuccess = (responseData) => {
+  $('#user-feedback').text('Begin Game')
+  store.game = responseData.game
+  $('#game-board').addClass('started')
+}
+
+const gameOver = (responseData) => {
+  $('#user-feedback').text('Game Over')
+  $('#game-board').removeClass('started')
 }
 
 const failure = (responseData) => {
-  $('#user-message').text('Something went wrong')
+  $('#user-feedback').text('Something went wrong')
 }
 
 const updateGameSuccess = (responseData) => {
-  $('#user-message').text('Game Updated')
+  $('#user-feedback').text('Game Updated')
 }
 
 // console.log('store:', store)
 module.exports = {
   createGameSuccess,
+  gameOver,
   updateGameSuccess,
   failure
 }
